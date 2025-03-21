90707efc524ac941c308b695b7c822ef90f0afdb
macongcong001 <macongcong001@ke.com>
2025-03-21 12:46:25 +0800

    UI调整

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -2,15 +2,11 @@ import type { Navigation } from "../types/navigation";
 
 export const NAVIGATION: Navigation = [
     {
-        name: "Home",
-        path: "/",
-    },
-    {
         name: "Blog",
         path: "/blog",
     },
     {
         name: "About",
         path: "/about",
-    },
+    }
 ]; 
