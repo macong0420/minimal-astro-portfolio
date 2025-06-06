a38effe728b5698219994f88dd7d460ab96b9c28
马聪聪 <macong0420@126.com>
2025-06-06 18:57:01 +0800

    左侧增加列表-调整

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -2,6 +2,10 @@ import type { Navigation } from "../types/navigation";
 
 export const NAVIGATION: Navigation = [
     {
+        name: "Home",
+        path: "/",
+    },
+    {
         name: "Blog",
         path: "/blog",
     },
@@ -9,4 +13,4 @@ export const NAVIGATION: Navigation = [
         name: "About",
         path: "/about",
     }
-]; 
+];
